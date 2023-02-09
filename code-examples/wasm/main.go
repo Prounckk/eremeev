b0f6456faa05bf38bc184583c77e0431b4983447
Sergei Eremeev <prounckk@gmail.com>
2023-02-09 16:32:25 -0500

    Removed SendGrid api key from wasm. moved logic to backend at CF Worker

--- code-examples/wasm/main.go
+++ code-examples/wasm/main.go
@@ -11,19 +11,19 @@ import (
 	"github.com/prounckk/eremeev/code-examples/wasm/dom"
 )
 
-var SENDGRID_API_KEY string
+var CF_API_KEY string
 
-var wg sync.WaitGroup // 1
+var wg sync.WaitGroup
 
 type Form struct {
-	email        string `json:"email"`
-	message      string `json:"message"`
-	name         string `json:"name"`
-	company      string `json:"company"`
-	companyniche string `json:"companyniche"`
-	position     string `json:"position"`
-	location     string `json:"location"`
-	salary       string `json:"salary"`
+	Email        string `json:"email"`
+	Message      string `json:"message"`
+	Name         string `json:"name"`
+	Company      string `json:"company"`
+	Companyniche string `json:"companyniche"`
+	Position     string `json:"position"`
+	Location     string `json:"location"`
+	Salary       string `json:"salary"`
 }
 
 func main() {
@@ -31,70 +31,38 @@ func main() {
 	wg.Add(1)
 	js.Global().Set("SubmitForm", js.FuncOf(SubmitForm))
 	wg.Wait()
-
 }
 
 func SubmitForm(this js.Value, args []js.Value) any {
 	form := Form{}
-	form.name = dom.GetStringFromElement("name")
-	form.email = dom.GetStringFromElement("email")
-	form.company = dom.GetStringFromElement("company")
-	form.companyniche = dom.GetStringFromElement("companyniche")
-	form.message = dom.GetStringFromElement("message")
-	form.position = dom.GetStringFromElement("position")
-	form.location = dom.GetStringFromElement("location")
-	form.salary = dom.GetStringFromElement("salary")
+	form.Name = dom.GetStringFromElement("name")
+	form.Email = dom.GetStringFromElement("email")
+	form.Company = dom.GetStringFromElement("company")
+	form.Companyniche = dom.GetStringFromElement("companyniche")
+	form.Message = dom.GetStringFromElement("message")
+	form.Position = dom.GetStringFromElement("position")
+	form.Location = dom.GetStringFromElement("location")
+	form.Salary = dom.GetStringFromElement("salary")
 
 	go form.sendEmail()
 	return nil
 }
 
 func (form Form) sendEmail() {
-	emailBody := map[string]interface{}{
-		"from": map[string]string{
-			"email": "me@eremeev.ca",
-		},
-		"personalizations": []map[string]interface{}{
-			{
-				"to": []map[string]string{
-					{
-						"email": "me@eremeev.ca",
-					},
-				},
-				"subject": form.location + " " + form.position,
-				"dynamic_template_data": map[string]string{
-					"sender":   form.email,
-					"name":     form.name,
-					"message":  form.message,
-					"subject":  form.location + " | " + form.position,
-					"company":  form.company,
-					"niche":    form.companyniche,
-					"position": form.position,
-					"salary":   form.salary,
-					"location": form.location,
-				},
-			},
-		},
-		"template_id": "d-6f8ef6f0f3ec4fc0bdae401d050babee",
-	}
-
-	body, err := json.Marshal(emailBody)
+	body, err := json.Marshal(form)
 	if err != nil {
 		fmt.Println(err)
 	}
-
-	req, err := http.NewRequest("POST", "https://api.sendgrid.com/v3/mail/send", bytes.NewBuffer(body))
+	req, err := http.NewRequest("POST", "https://eremeev.ca/contact-form", bytes.NewBuffer(body))
 	if err != nil {
 		fmt.Println(err)
 	}
-	req.Header.Set("Authorization", "Bearer "+SENDGRID_API_KEY)
+	req.Header.Set("Authorization", "Bearer "+CF_API_KEY)
 	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("User-Agent", "sendgrid/3.12.0;go")
-	req.Header.Set("Accept", "application/json")
-
 	client := &http.Client{}
 	defer wg.Done()
 	res, err := client.Do(req)
+
 	if err != nil {
 		fmt.Println(err)
 		return
